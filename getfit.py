f9ccf5d7d36149b608d216ac8afe10bc4e320c2e
sho41052 <suzuhei@gmail.com>
2022-07-10 15:38:32 +0900

    get data

---
+++ getfit.py
@@ -0,0 +1,81 @@
+import os
+import itertools
+import pandas as pd
+import pathlib
+from google_fit_api import google_fit_api
+from datetime import datetime, timedelta
+
+
+def get_data():
+    fitness_service = google_fit_api()
+
+    datasource_step = 'derived:com.google.step_count.delta:com.google.android.gms:estimated_steps'
+    datatype_step = 'com.google.step_count.delta'
+    datasource_weight = 'derived:com.google.weight:com.google.android.gms:merge_weight'
+    datatype_weight = 'merge_weight'
+    datasource_fat = 'derived:com.google.body.fat.percentage:com.google.android.gms:merged'
+    datatype_fat = 'com.google.body.fat.percentage'
+
+    # preparing data frame
+    cols = ['day', 'weight', 'step', 'fat']
+    df = pd.DataFrame(index=[], columns=cols)
+    d1 = datetime(2021, 11, 1).date()
+    d2 = datetime.today().date()
+    for i in range((d2 - d1).days + 1):
+        date = d1 + timedelta(i)
+        record = pd.Series([date, None, None, None], index=df.columns, name=date)
+        df.loc[len(df)] = record
+    df = df.set_index('day')
+
+    min_start = datetime(2021, 11, 1)
+
+    # load previous data
+    if os.path.exists('data/main_data.pkl'):
+        last_update = datetime.fromtimestamp(pathlib.Path('data/main_data.pkl').stat().st_mtime).date()
+        if last_update == datetime.today().date():
+            df = pd.read_pickle('data/main_data.pkl')
+            min_start = datetime.today()# - timedelta(days=1)
+
+    for i in itertools.count():
+        start_day = datetime.today() - timedelta(days=30) * (i + 1)
+        if start_day < min_start:
+            start_day = min_start
+        end_day = datetime.today() - timedelta(days=30) * i
+
+        step_result = fitness_service.get_data_multi_days(datasource_step, datatype_step, start_day, end_day)
+        weight_result = fitness_service.get_data_multi_days(datasource_weight, datatype_weight, start_day, end_day)
+        fat_result = fitness_service.get_data_multi_days(datasource_fat, datatype_fat, start_day, end_day)
+        for daily_data in step_result['bucket']:
+            start = datetime.fromtimestamp(int(daily_data['startTimeMillis']) / 1000).date()
+            try:
+                step = int(daily_data['dataset'][0]['point'][0]['value'][0]['intVal'])
+            except IndexError as e:
+                step = 0
+            df.at[start, 'step'] = step
+
+        for daily_data in weight_result['bucket']:
+            start = datetime.fromtimestamp(int(daily_data['startTimeMillis']) / 1000).date()
+            try:
+                weight = float(daily_data['dataset'][0]['point'][0]['value'][0]['fpVal'])
+            except IndexError as e:
+                continue
+            df.at[start, 'weight'] = weight
+
+        for daily_data in fat_result['bucket']:
+            start = datetime.fromtimestamp(int(daily_data['startTimeMillis']) / 1000).date()
+            try:
+                fat = float(daily_data['dataset'][0]['point'][0]['value'][0]['fpVal'])
+            except IndexError as e:
+                continue
+            df.at[start, 'fat'] = fat
+
+        if start_day == min_start:
+            break
+
+    df.to_pickle('data/main_data.pkl')
+    df.to_csv('data/main_data.csv')
+    return df
+
+
+if __name__ == "__main__":
+    get_data()
---
+++ gui_window.py
@@ -0,0 +1,92 @@
+import logging
+import sys
+
+from datetime import datetime
+from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, qApp, QStackedWidget, QMenu, QActionGroup
+
+from weight_fat_graph import weight_fat_graph
+from weight_fat_input import weight_fat_input
+
+logging.basicConfig(level=logging.INFO)
+
+
+class MainWindow(QMainWindow):
+
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        # set title
+        self.setWindowTitle('Google fit weight / fat')
+
+        # MenuBar
+        menubar = self.menuBar()
+
+        # File Menu
+        fileMenu = menubar.addMenu('File')
+        # Save pic in File
+        savePicAction = QAction('Save picture', self)
+        savePicAction.triggered.connect(self.save_picture)
+        fileMenu.addAction(savePicAction)
+        # Update graph data
+        updateGraphData = QAction('Update graph', self)
+        updateGraphData.triggered.connect(self.update_graph_data)
+        fileMenu.addAction(updateGraphData)
+        # Exit in File
+        exitAction = QAction('Exit', self)
+        exitAction.triggered.connect(qApp.quit)
+        fileMenu.addAction(exitAction)
+
+        # Mode Menu
+        '''
+        https://stackoverflow.com/questions/48447053/one-qaction-checkable-at-time-in-qmenu
+        '''
+        modeType = QMenu('Mode', self)
+        group = QActionGroup(modeType)
+        texts = ['graph', 'insert']
+        for text in texts:
+            action = QAction(text, modeType, checkable=True, checked=(text == texts[0]))
+            modeType.addAction(action)
+            group.addAction(action)
+        group.setExclusive(True)
+        group.triggered.connect(self.mode_change)
+        menubar.addMenu(modeType)
+
+        # Modes in Mode
+        self.widget_weight_fat = weight_fat_graph()
+        self.widget_weight_input = weight_fat_input()
+
+        self.Stack = QStackedWidget(self)
+        self.Stack.addWidget(self.widget_weight_input)
+        self.Stack.addWidget(self.widget_weight_fat)
+        self.Stack.setCurrentIndex(1)
+        self.resize(1000, 600)
+        self.setCentralWidget(self.Stack)
+
+        self.show()
+
+    def mode_change(self, mode):
+        if mode.text() == 'insert':
+            self.Stack.setCurrentIndex(0)
+            self.resize(100, 100)
+        elif mode.text() == 'graph':
+            self.Stack.setCurrentIndex(1)
+            self.resize(1000, 600)
+        else:
+            '''
+            New feature
+            '''
+
+    def save_picture(self):
+        file_name = str(datetime.now().date()) + '_' + str(datetime.now().time())
+        self.widget_weight_fat.save_picture(file_name)
+        print(file_name)
+
+    def update_graph_data(self):
+        self.widget_weight_fat.update_graph_data()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    win = MainWindow()
+    win.show()
+    sys.exit(app.exec_())
